b5a805da6875bfadc59135c5bed186dab93e11e9
Cyan <snow22314@outlook.com>
2022-02-18 15:56:29 +0800

    fix [Sonatype Lift] EqualsGetClass #22

--- rest-toolkit-starter/src/main/java/io/github/nichetoolkit/rest/identity/worker/IdentityWorkerArtificial.java
+++ rest-toolkit-starter/src/main/java/io/github/nichetoolkit/rest/identity/worker/IdentityWorkerArtificial.java
@@ -75,7 +75,8 @@ class IdentityWorkerArtificial implements IdentityWorker {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (o == null) return false;
+        if (! (o instanceof IdentityWorkerArtificial)) return false;
         IdentityWorkerArtificial that = (IdentityWorkerArtificial) o;
         return Objects.equals(name, that.name);
     }
