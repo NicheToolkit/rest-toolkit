eae8458d45795d2e84ad379ea4704c84cac8fea3
Cyan <snow22314@outlook.com>
2023-01-05 14:06:20 +0800

    fix

--- rest-toolkit-utils/src/main/java/io/github/nichetoolkit/rest/configure/JwtAutoConfigure.java
+++ rest-toolkit-utils/src/main/java/io/github/nichetoolkit/rest/configure/JwtAutoConfigure.java
@@ -3,27 +3,28 @@ package io.github.nichetoolkit.rest.configure;
 import io.github.nichetoolkit.rest.worker.jwt.JwtWorker;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Primary;
 
 /**
  * <p>JwtAutoConfigure</p>
- *
  * @author Cyan (snow22314@outlook.com)
  * @version v1.0.0
  */
 @Slf4j
-@AutoConfiguration
-@ComponentScan(basePackages = {"io.github.nichetoolkit.rest"})
-@ConditionalOnProperty(value = "nichetoolkit.rest.radix.enabled", havingValue = "true")
+@ConditionalOnClass({JwtWorker.class})
+@AutoConfiguration(after = {RestUtilsAutoConfigure.class,RadixAutoConfigure.class})
+@EnableConfigurationProperties({RestJwtProperties.class})
 public class JwtAutoConfigure {
 
     @Bean
     @Primary
     @ConditionalOnMissingBean(JwtWorker.class)
+    @ConditionalOnProperty(value = "nichetoolkit.rest.radix.enabled", havingValue = "true")
     public JwtWorker jwtWorker(RestJwtProperties jwtProperties) {
         return new JwtWorker(jwtProperties);
     }
--- rest-toolkit-utils/src/main/java/io/github/nichetoolkit/rest/configure/RadixAutoConfigure.java
+++ rest-toolkit-utils/src/main/java/io/github/nichetoolkit/rest/configure/RadixAutoConfigure.java
@@ -3,30 +3,28 @@ package io.github.nichetoolkit.rest.configure;
 import io.github.nichetoolkit.rest.worker.RadixWorker;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Primary;
 
 /**
  * <p>RadixAutoConfigure</p>
- *
  * @author Cyan (snow22314@outlook.com)
  * @version v1.0.0
  */
 @Slf4j
 @AutoConfiguration
-@ComponentScan(basePackages = {"io.github.nichetoolkit.rest"})
-@AutoConfigureBefore(value = {JwtAutoConfigure.class})
-@ConditionalOnProperty(value = "nichetoolkit.rest.radix.enabled", havingValue = "true")
+@ConditionalOnClass({RadixWorker.class})
+@EnableConfigurationProperties({RestRadixProperties.class})
 public class RadixAutoConfigure {
 
     @Bean
     @Primary
     @ConditionalOnMissingBean(RadixWorker.class)
+    @ConditionalOnProperty(value = "nichetoolkit.rest.radix.enabled", havingValue = "true")
     public RadixWorker radixWorker(RestRadixProperties radixProperties) {
         return new RadixWorker(radixProperties);
     }
