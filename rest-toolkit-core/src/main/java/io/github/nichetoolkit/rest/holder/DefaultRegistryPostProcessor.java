c94d6af2934fd0d5e700232c0eee1107e6d7e1c2
Cyan <snow22314@outlook.com>
2024-10-08 00:09:14 +0800

    ListableBeanFactoryHolder

--- rest-toolkit-core/src/main/java/io/github/nichetoolkit/rest/holder/DefaultRegistryPostProcessor.java
+++ rest-toolkit-core/src/main/java/io/github/nichetoolkit/rest/holder/DefaultRegistryPostProcessor.java
@@ -24,22 +24,28 @@ import java.util.List;
 public class DefaultRegistryPostProcessor implements BeanDefinitionRegistryPostProcessor {
 
     @Override
-    @SuppressWarnings("rawtypes")
+
     public void postProcessBeanDefinitionRegistry(@NonNull BeanDefinitionRegistry beanDefinitionRegistry) throws BeansException {
         BeanDefinitionRegistryHolder.initBeanDefinitionRegistry(beanDefinitionRegistry);
-        List<RestIntend> intendList = SpringFactoriesLoader.loadFactories(RestIntend.class, null);
-        if (GeneralUtils.isNotEmpty(intendList)) {
-            for (RestIntend<?> intend : intendList) {
-                String beanName = intend.beanName();
-                Class<?> beanType = intend.beanType();
-                BeanDefinitionRegistryHolder.registerBeanDefinition(beanName, beanType);
-            }
-            log.debug("There are {} intend beans has be initiated.", intendList.size());
-        }
     }
 
     @Override
     public void postProcessBeanFactory(@NonNull ConfigurableListableBeanFactory listableBeanFactory) throws BeansException {
         ListableBeanFactoryHolder.initConfigurableListableBeanFactory(listableBeanFactory);
+        intendBeanRegistryAndAutowireProperties();
+    }
+
+    @SuppressWarnings("rawtypes")
+    private void intendBeanRegistryAndAutowireProperties() throws BeansException {
+        List<RestIntend> intendList = SpringFactoriesLoader.loadFactories(RestIntend.class, null);
+        if (GeneralUtils.isNotEmpty(intendList)) {
+            for (RestIntend<?> intend : intendList) {
+                String beanName = intend.beanName();
+                Class<?> beanType = intend.beanType();
+                Object object = BeanDefinitionRegistryHolder.registerBeanDefinition(beanName, beanType);
+                ListableBeanFactoryHolder.autowireBeanProperties(object);
+            }
+            log.debug("There are {} intend beans has be initiated.", intendList.size());
+        }
     }
 }
--- rest-toolkit-core/src/main/java/io/github/nichetoolkit/rest/holder/ListableBeanFactoryHolder.java
+++ rest-toolkit-core/src/main/java/io/github/nichetoolkit/rest/holder/ListableBeanFactoryHolder.java
@@ -1,6 +1,8 @@
 package io.github.nichetoolkit.rest.holder;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.lang.NonNull;
 
@@ -43,6 +45,161 @@ public class ListableBeanFactoryHolder {
     @NonNull
     public static ConfigurableListableBeanFactory getConfigurableListableBeanFactory() {
         return LISTABLE_BEAN_FACTORY;
+    }
+
+
+    /**
+     * <code>createBean</code>
+     * <p>The bean method.</p>
+     * @param <T>      {@link java.lang.Object} <p>The parameter can be of any type.</p>
+     * @param beanType {@link java.lang.Class} <p>The bean type parameter is <code>Class</code> type.</p>
+     * @return T <p>The bean return object is <code>T</code> type.</p>
+     * @throws BeansException {@link org.springframework.beans.BeansException} <p>The beans exception is <code>BeansException</code> type.</p>
+     * @see java.lang.Class
+     * @see org.springframework.beans.BeansException
+     */
+    public static <T> T createBean(Class<T> beanType) throws BeansException {
+        return LISTABLE_BEAN_FACTORY.createBean(beanType);
+    }
+
+    /**
+     * <code>autowireBean</code>
+     * <p>The bean method.</p>
+     * @param existingBean {@link java.lang.Object} <p>The existing bean parameter is <code>Object</code> type.</p>
+     * @throws BeansException {@link org.springframework.beans.BeansException} <p>The beans exception is <code>BeansException</code> type.</p>
+     * @see java.lang.Object
+     * @see org.springframework.beans.BeansException
+     */
+    public static void autowireBean(Object existingBean) throws BeansException {
+        LISTABLE_BEAN_FACTORY.autowireBean(existingBean);
+    }
+
+    /**
+     * <code>configureBean</code>
+     * <p>The bean method.</p>
+     * @param existingBean {@link java.lang.Object} <p>The existing bean parameter is <code>Object</code> type.</p>
+     * @param beanName     {@link java.lang.String} <p>The bean name parameter is <code>String</code> type.</p>
+     * @return {@link java.lang.Object} <p>The bean return object is <code>Object</code> type.</p>
+     * @throws BeansException {@link org.springframework.beans.BeansException} <p>The beans exception is <code>BeansException</code> type.</p>
+     * @see java.lang.Object
+     * @see java.lang.String
+     * @see org.springframework.beans.BeansException
+     */
+    public static Object configureBean(Object existingBean, String beanName) throws BeansException {
+        return LISTABLE_BEAN_FACTORY.configureBean(existingBean, beanName);
+    }
+
+    /**
+     * <code>createBean</code>
+     * <p>The bean method.</p>
+     * @param beanType        {@link java.lang.Class} <p>The bean type parameter is <code>Class</code> type.</p>
+     * @param autowireMode    int <p>The autowire mode parameter is <code>int</code> type.</p>
+     * @param dependencyCheck boolean <p>The dependency check parameter is <code>boolean</code> type.</p>
+     * @return {@link java.lang.Object} <p>The bean return object is <code>Object</code> type.</p>
+     * @throws BeansException {@link org.springframework.beans.BeansException} <p>The beans exception is <code>BeansException</code> type.</p>
+     * @see java.lang.Class
+     * @see java.lang.Object
+     * @see org.springframework.beans.BeansException
+     */
+    public static Object createBean(Class<?> beanType, int autowireMode, boolean dependencyCheck) throws BeansException {
+        return LISTABLE_BEAN_FACTORY.createBean(beanType, autowireMode, dependencyCheck);
+    }
+
+
+    /**
+     * <code>autowire</code>
+     * <p>The method.</p>
+     * @param beanType {@link java.lang.Class} <p>The bean type parameter is <code>Class</code> type.</p>
+     * @return {@link java.lang.Object} <p>The return object is <code>Object</code> type.</p>
+     * @throws BeansException {@link org.springframework.beans.BeansException} <p>The beans exception is <code>BeansException</code> type.</p>
+     * @see java.lang.Class
+     * @see java.lang.Object
+     * @see org.springframework.beans.BeansException
+     */
+    public static Object autowire(Class<?> beanType) throws BeansException {
+        return LISTABLE_BEAN_FACTORY.autowire(beanType, AutowireCapableBeanFactory.AUTOWIRE_BY_TYPE, true);
+    }
+
+
+    /**
+     * <code>autowire</code>
+     * <p>The method.</p>
+     * @param beanType        {@link java.lang.Class} <p>The bean type parameter is <code>Class</code> type.</p>
+     * @param autowireMode    int <p>The autowire mode parameter is <code>int</code> type.</p>
+     * @param dependencyCheck boolean <p>The dependency check parameter is <code>boolean</code> type.</p>
+     * @return {@link java.lang.Object} <p>The return object is <code>Object</code> type.</p>
+     * @throws BeansException {@link org.springframework.beans.BeansException} <p>The beans exception is <code>BeansException</code> type.</p>
+     * @see java.lang.Class
+     * @see java.lang.Object
+     * @see org.springframework.beans.BeansException
+     */
+    public static Object autowire(Class<?> beanType, int autowireMode, boolean dependencyCheck) throws BeansException {
+        return LISTABLE_BEAN_FACTORY.autowire(beanType, autowireMode, dependencyCheck);
+    }
+
+    /**
+     * <code>autowireBeanProperties</code>
+     * <p>The bean properties method.</p>
+     * @param existingBean {@link java.lang.Object} <p>The existing bean parameter is <code>Object</code> type.</p>
+     * @throws BeansException {@link org.springframework.beans.BeansException} <p>The beans exception is <code>BeansException</code> type.</p>
+     * @see java.lang.Object
+     * @see org.springframework.beans.BeansException
+     */
+    public static void autowireBeanProperties(Object existingBean) throws BeansException {
+        LISTABLE_BEAN_FACTORY.autowireBeanProperties(existingBean, AutowireCapableBeanFactory.AUTOWIRE_BY_TYPE, true);
+    }
+
+    /**
+     * <code>autowireBeanProperties</code>
+     * <p>The bean properties method.</p>
+     * @param existingBean    {@link java.lang.Object} <p>The existing bean parameter is <code>Object</code> type.</p>
+     * @param autowireMode    int <p>The autowire mode parameter is <code>int</code> type.</p>
+     * @param dependencyCheck boolean <p>The dependency check parameter is <code>boolean</code> type.</p>
+     * @throws BeansException {@link org.springframework.beans.BeansException} <p>The beans exception is <code>BeansException</code> type.</p>
+     * @see java.lang.Object
+     * @see org.springframework.beans.BeansException
+     */
+    public static void autowireBeanProperties(Object existingBean, int autowireMode, boolean dependencyCheck) throws BeansException {
+        LISTABLE_BEAN_FACTORY.autowireBeanProperties(existingBean, autowireMode, dependencyCheck);
+    }
+
+    /**
+     * <code>applyBeanPropertyValues</code>
+     * <p>The bean property values method.</p>
+     * @param existingBean {@link java.lang.Object} <p>The existing bean parameter is <code>Object</code> type.</p>
+     * @param beanName     {@link java.lang.String} <p>The bean name parameter is <code>String</code> type.</p>
+     * @throws BeansException {@link org.springframework.beans.BeansException} <p>The beans exception is <code>BeansException</code> type.</p>
+     * @see java.lang.Object
+     * @see java.lang.String
+     * @see org.springframework.beans.BeansException
+     */
+    public static void applyBeanPropertyValues(Object existingBean, String beanName) throws BeansException {
+        LISTABLE_BEAN_FACTORY.applyBeanPropertyValues(existingBean, beanName);
+    }
+
+    /**
+     * <code>initializeBean</code>
+     * <p>The bean method.</p>
+     * @param existingBean {@link java.lang.Object} <p>The existing bean parameter is <code>Object</code> type.</p>
+     * @param beanName     {@link java.lang.String} <p>The bean name parameter is <code>String</code> type.</p>
+     * @return {@link java.lang.Object} <p>The bean return object is <code>Object</code> type.</p>
+     * @throws BeansException {@link org.springframework.beans.BeansException} <p>The beans exception is <code>BeansException</code> type.</p>
+     * @see java.lang.Object
+     * @see java.lang.String
+     * @see org.springframework.beans.BeansException
+     */
+    public static Object initializeBean(Object existingBean, String beanName) throws BeansException {
+        return LISTABLE_BEAN_FACTORY.initializeBean(existingBean, beanName);
+    }
+
+    /**
+     * <code>destroyBean</code>
+     * <p>The bean method.</p>
+     * @param existingBean {@link java.lang.Object} <p>The existing bean parameter is <code>Object</code> type.</p>
+     * @see java.lang.Object
+     */
+    public static void destroyBean(Object existingBean) {
+        LISTABLE_BEAN_FACTORY.destroyBean(existingBean);
     }
 
 
