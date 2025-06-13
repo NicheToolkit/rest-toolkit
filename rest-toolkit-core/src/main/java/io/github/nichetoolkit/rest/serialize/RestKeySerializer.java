45b344b2f916e138cc8295789faa01d3ab69ec3b
Cyan <snow22314@outlook.com>
2025-06-13 11:53:15 +0800

    Serializer

---
+++ rest-toolkit-core/src/main/java/io/github/nichetoolkit/rest/serialize/RestKeySerializer.java
@@ -0,0 +1,26 @@
+package io.github.nichetoolkit.rest.serialize;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import io.github.nichetoolkit.rest.RestKey;
+import io.github.nichetoolkit.rest.util.GeneralUtils;
+
+import java.io.IOException;
+
+/**
+ * <code>RestKeySerializer</code>
+ * <p>The rest key serializer class.</p>
+ * @author Cyan (snow22314@outlook.com)
+ * @see com.fasterxml.jackson.databind.JsonSerializer
+ * @since Jdk1.8
+ */
+public class RestKeySerializer extends JsonSerializer<RestKey<?>> {
+    @Override
+    public void serialize(RestKey<?> restKey, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws
+    IOException {
+        if (GeneralUtils.isNotEmpty(restKey)) {
+            jsonGenerator.writeObject(restKey.getKey());
+        }
+    }
+}
---
+++ rest-toolkit-core/src/main/java/io/github/nichetoolkit/rest/serialize/RestValueSerializer.java
@@ -0,0 +1,26 @@
+package io.github.nichetoolkit.rest.serialize;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import io.github.nichetoolkit.rest.RestValue;
+import io.github.nichetoolkit.rest.util.GeneralUtils;
+
+import java.io.IOException;
+
+/**
+ * <code>RestValueSerializer</code>
+ * <p>The rest value serializer class.</p>
+ * @author Cyan (snow22314@outlook.com)
+ * @see com.fasterxml.jackson.databind.JsonSerializer
+ * @since Jdk1.8
+ */
+public class RestValueSerializer extends JsonSerializer<RestValue<?,?>> {
+    @Override
+    public void serialize(RestValue<?,?> restValue, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws
+    IOException {
+        if (GeneralUtils.isNotEmpty(restValue)) {
+            jsonGenerator.writeObject(restValue.getValue());
+        }
+    }
+}
