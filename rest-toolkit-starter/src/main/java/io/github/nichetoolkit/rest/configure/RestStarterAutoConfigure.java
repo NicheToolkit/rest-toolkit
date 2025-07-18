3300167fe8e3acaf30ec5f0c794ff3baab1efa76
Cyan <snow22314@outlook.com>
2025-07-18 13:33:08 +0800

    fix

--- rest-toolkit-starter/src/main/java/io/github/nichetoolkit/rest/configure/RestStarterAutoConfigure.java
+++ rest-toolkit-starter/src/main/java/io/github/nichetoolkit/rest/configure/RestStarterAutoConfigure.java
@@ -8,8 +8,6 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.annotation.Order;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
 /**
  * <code>RestStarterAutoConfigure</code>
@@ -36,8 +34,8 @@ public class RestStarterAutoConfigure {
     }
 
     @Bean
-    @Order(0)
-    @ConditionalOnMissingBean(ResponseBodyAdvice.class)
+    @Order(Integer.MIN_VALUE)
+    @ConditionalOnMissingBean(DefaultControllerAdvice.class)
     public DefaultControllerAdvice controllerAdvice(RestExceptionProperties errorProperties) {
         return new DefaultControllerAdvice(errorProperties);
     }
