24fa03c79ac394ff27560dc579d167391d853a9f
Cyan <snow22314@outlook.com>
2025-07-18 11:58:29 +0800

    fix

--- rest-toolkit-starter/src/main/java/io/github/nichetoolkit/rest/configure/RestHttpAutoConfigure.java
+++ rest-toolkit-starter/src/main/java/io/github/nichetoolkit/rest/configure/RestHttpAutoConfigure.java
@@ -34,8 +34,6 @@ import org.apache.http.protocol.HTTP;
 import org.apache.http.ssl.SSLContextBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
