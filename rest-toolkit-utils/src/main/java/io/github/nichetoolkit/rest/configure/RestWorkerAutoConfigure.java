8c0a5e44073dc344685e4893b4d769dac3b5d2a4
Cyan <snow22314@outlook.com>
2025-09-02 14:23:24 +0800

    javadoc

--- rest-toolkit-utils/src/main/java/io/github/nichetoolkit/rest/configure/RestWorkerAutoConfigure.java
+++ rest-toolkit-utils/src/main/java/io/github/nichetoolkit/rest/configure/RestWorkerAutoConfigure.java
@@ -14,18 +14,67 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 
+/**
+ * <code>RestWorkerAutoConfigure</code>
+ * <p>The rest worker auto configure class.</p>
+ * @author Cyan (snow22314@outlook.com)
+ * @see lombok.extern.slf4j.Slf4j
+ * @see org.springframework.boot.autoconfigure.AutoConfiguration
+ * @see org.springframework.boot.autoconfigure.AutoConfigureAfter
+ * @see org.springframework.boot.context.properties.EnableConfigurationProperties
+ * @since Jdk1.8
+ */
 @Slf4j
 @AutoConfiguration
 @AutoConfigureAfter(RestCoreAutoConfigure.class)
 @EnableConfigurationProperties({RestJwtProperties.class,RestRadixProperties.class,RestRsaProperties.class,RestShaProperties.class,RestAesProperties.class})
 public class RestWorkerAutoConfigure {
 
+    /**
+     * <code>jwtProperties</code>
+     * {@link io.github.nichetoolkit.rest.configure.RestJwtProperties} <p>The <code>jwtProperties</code> field.</p>
+     * @see io.github.nichetoolkit.rest.configure.RestJwtProperties
+     */
     private final RestJwtProperties jwtProperties;
+    /**
+     * <code>radixProperties</code>
+     * {@link io.github.nichetoolkit.rest.configure.RestRadixProperties} <p>The <code>radixProperties</code> field.</p>
+     * @see io.github.nichetoolkit.rest.configure.RestRadixProperties
+     */
     private final RestRadixProperties radixProperties;
+    /**
+     * <code>shaProperties</code>
+     * {@link io.github.nichetoolkit.rest.configure.RestShaProperties} <p>The <code>shaProperties</code> field.</p>
+     * @see io.github.nichetoolkit.rest.configure.RestShaProperties
+     */
     private final RestShaProperties shaProperties;
+    /**
+     * <code>rsaProperties</code>
+     * {@link io.github.nichetoolkit.rest.configure.RestRsaProperties} <p>The <code>rsaProperties</code> field.</p>
+     * @see io.github.nichetoolkit.rest.configure.RestRsaProperties
+     */
     private final RestRsaProperties rsaProperties;
+    /**
+     * <code>aesProperties</code>
+     * {@link io.github.nichetoolkit.rest.configure.RestAesProperties} <p>The <code>aesProperties</code> field.</p>
+     * @see io.github.nichetoolkit.rest.configure.RestAesProperties
+     */
     private final RestAesProperties aesProperties;
 
+    /**
+     * <code>RestWorkerAutoConfigure</code>
+     * <p>Instantiates a new rest worker auto configure.</p>
+     * @param jwtProperties   {@link io.github.nichetoolkit.rest.configure.RestJwtProperties} <p>The jwt properties parameter is <code>RestJwtProperties</code> type.</p>
+     * @param radixProperties {@link io.github.nichetoolkit.rest.configure.RestRadixProperties} <p>The radix properties parameter is <code>RestRadixProperties</code> type.</p>
+     * @param shaProperties   {@link io.github.nichetoolkit.rest.configure.RestShaProperties} <p>The sha properties parameter is <code>RestShaProperties</code> type.</p>
+     * @param rsaProperties   {@link io.github.nichetoolkit.rest.configure.RestRsaProperties} <p>The rsa properties parameter is <code>RestRsaProperties</code> type.</p>
+     * @param aesProperties   {@link io.github.nichetoolkit.rest.configure.RestAesProperties} <p>The aes properties parameter is <code>RestAesProperties</code> type.</p>
+     * @see io.github.nichetoolkit.rest.configure.RestJwtProperties
+     * @see io.github.nichetoolkit.rest.configure.RestRadixProperties
+     * @see io.github.nichetoolkit.rest.configure.RestShaProperties
+     * @see io.github.nichetoolkit.rest.configure.RestRsaProperties
+     * @see io.github.nichetoolkit.rest.configure.RestAesProperties
+     */
     public RestWorkerAutoConfigure(RestJwtProperties jwtProperties, RestRadixProperties radixProperties, RestShaProperties shaProperties, RestRsaProperties rsaProperties, RestAesProperties aesProperties) {
         this.jwtProperties = jwtProperties;
         this.radixProperties = radixProperties;
@@ -35,6 +84,16 @@ public class RestWorkerAutoConfigure {
         log.debug("The auto configuration for [rest-worker] initiated");
     }
 
+    /**
+     * <code>radixWorker</code>
+     * <p>The radix worker method.</p>
+     * @return {@link io.github.nichetoolkit.rest.worker.RadixWorker} <p>The radix worker return object is <code>RadixWorker</code> type.</p>
+     * @see io.github.nichetoolkit.rest.worker.RadixWorker
+     * @see org.springframework.context.annotation.Bean
+     * @see org.springframework.context.annotation.Primary
+     * @see org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
+     * @see org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+     */
     @Bean
     @Primary
     @ConditionalOnMissingBean(RadixWorker.class)
@@ -43,6 +102,16 @@ public class RestWorkerAutoConfigure {
         return new RadixWorker(this.radixProperties);
     }
 
+    /**
+     * <code>jwtWorker</code>
+     * <p>The jwt worker method.</p>
+     * @return {@link io.github.nichetoolkit.rest.worker.jwt.JwtWorker} <p>The jwt worker return object is <code>JwtWorker</code> type.</p>
+     * @see io.github.nichetoolkit.rest.worker.jwt.JwtWorker
+     * @see org.springframework.context.annotation.Bean
+     * @see org.springframework.context.annotation.Primary
+     * @see org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
+     * @see org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+     */
     @Bean
     @Primary
     @ConditionalOnMissingBean(JwtWorker.class)
@@ -51,6 +120,16 @@ public class RestWorkerAutoConfigure {
         return new JwtWorker(this.jwtProperties);
     }
 
+    /**
+     * <code>shaWorker</code>
+     * <p>The sha worker method.</p>
+     * @return {@link io.github.nichetoolkit.rest.worker.sha.ShaWorker} <p>The sha worker return object is <code>ShaWorker</code> type.</p>
+     * @see io.github.nichetoolkit.rest.worker.sha.ShaWorker
+     * @see org.springframework.context.annotation.Bean
+     * @see org.springframework.context.annotation.Primary
+     * @see org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
+     * @see org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+     */
     @Bean
     @Primary
     @ConditionalOnMissingBean(ShaWorker.class)
@@ -59,6 +138,16 @@ public class RestWorkerAutoConfigure {
         return new ShaWorker(this.shaProperties);
     }
 
+    /**
+     * <code>rsaWorker</code>
+     * <p>The rsa worker method.</p>
+     * @return {@link io.github.nichetoolkit.rest.worker.rsa.RsaWorker} <p>The rsa worker return object is <code>RsaWorker</code> type.</p>
+     * @see io.github.nichetoolkit.rest.worker.rsa.RsaWorker
+     * @see org.springframework.context.annotation.Bean
+     * @see org.springframework.context.annotation.Primary
+     * @see org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
+     * @see org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+     */
     @Bean
     @Primary
     @ConditionalOnMissingBean(RsaWorker.class)
@@ -67,6 +156,16 @@ public class RestWorkerAutoConfigure {
         return new RsaWorker(this.rsaProperties);
     }
 
+    /**
+     * <code>aesWorker</code>
+     * <p>The aes worker method.</p>
+     * @return {@link io.github.nichetoolkit.rest.worker.AesWorker} <p>The aes worker return object is <code>AesWorker</code> type.</p>
+     * @see io.github.nichetoolkit.rest.worker.AesWorker
+     * @see org.springframework.context.annotation.Bean
+     * @see org.springframework.context.annotation.Primary
+     * @see org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
+     * @see org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+     */
     @Bean
     @Primary
     @ConditionalOnMissingBean(AesWorker.class)
--- rest-toolkit-utils/src/main/java/io/github/nichetoolkit/rest/worker/AesWorker.java
+++ rest-toolkit-utils/src/main/java/io/github/nichetoolkit/rest/worker/AesWorker.java
@@ -17,52 +17,142 @@ import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 
+/**
+ * <code>AesWorker</code>
+ * <p>The aes worker class.</p>
+ * @author Cyan (snow22314@outlook.com)
+ * @see lombok.extern.slf4j.Slf4j
+ * @since Jdk1.8
+ */
 @Slf4j
 public class AesWorker {
 
+    /**
+     * <code>TRANSFORMATION</code>
+     * {@link java.lang.String} <p>The constant <code>TRANSFORMATION</code> field.</p>
+     * @see java.lang.String
+     */
     private static final String TRANSFORMATION = "AES/CBC/PKCS5Padding";
 
+    /**
+     * <code>ALGORITHM</code>
+     * {@link java.lang.String} <p>The constant <code>ALGORITHM</code> field.</p>
+     * @see java.lang.String
+     */
     private static final String ALGORITHM = "AES";
 
+    /**
+     * <code>aesProperties</code>
+     * {@link io.github.nichetoolkit.rest.configure.RestAesProperties} <p>The <code>aesProperties</code> field.</p>
+     * @see io.github.nichetoolkit.rest.configure.RestAesProperties
+     */
     private final RestAesProperties aesProperties;
 
+    /**
+     * <code>INSTANCE</code>
+     * {@link io.github.nichetoolkit.rest.worker.AesWorker} <p>The constant <code>INSTANCE</code> field.</p>
+     */
     private static AesWorker INSTANCE = null;
 
+    /**
+     * <code>getInstance</code>
+     * <p>The get instance getter method.</p>
+     * @return {@link io.github.nichetoolkit.rest.worker.AesWorker} <p>The get instance return object is <code>AesWorker</code> type.</p>
+     */
     public static AesWorker getInstance() {
         return INSTANCE;
     }
 
+    /**
+     * <code>AesWorker</code>
+     * <p>Instantiates a new aes worker.</p>
+     * @param aesProperties {@link io.github.nichetoolkit.rest.configure.RestAesProperties} <p>The aes properties parameter is <code>RestAesProperties</code> type.</p>
+     * @see io.github.nichetoolkit.rest.configure.RestAesProperties
+     * @see org.springframework.beans.factory.annotation.Autowired
+     */
     @Autowired
     public AesWorker(RestAesProperties aesProperties) {
         this.aesProperties = aesProperties;
     }
 
+    /**
+     * <code>radixWorkerInit</code>
+     * <p>The radix worker init method.</p>
+     * @see javax.annotation.PostConstruct
+     */
     @PostConstruct
     public void radixWorkerInit() {
         log.debug("The aes      properties: {}", JsonUtils.parseJson(aesProperties));
         INSTANCE = this;
     }
 
+    /**
+     * <code>encrypt</code>
+     * <p>The encrypt method.</p>
+     * @param source {@link java.lang.String} <p>The source parameter is <code>String</code> type.</p>
+     * @return {@link java.lang.String} <p>The encrypt return object is <code>String</code> type.</p>
+     * @see java.lang.String
+     */
     public String encrypt(String source) {
         return encrypts(source, aesProperties);
     }
 
+    /**
+     * <code>decrypt</code>
+     * <p>The decrypt method.</p>
+     * @param target {@link java.lang.String} <p>The target parameter is <code>String</code> type.</p>
+     * @return {@link java.lang.String} <p>The decrypt return object is <code>String</code> type.</p>
+     * @see java.lang.String
+     */
     public String decrypt(String target) {
         return decrypts(target, aesProperties);
     }
 
+    /**
+     * <code>encrypts</code>
+     * <p>The encrypts method.</p>
+     * @param source     {@link java.lang.String} <p>The source parameter is <code>String</code> type.</p>
+     * @param properties {@link io.github.nichetoolkit.rest.configure.RestAesProperties} <p>The properties parameter is <code>RestAesProperties</code> type.</p>
+     * @return {@link java.lang.String} <p>The encrypts return object is <code>String</code> type.</p>
+     * @see java.lang.String
+     * @see io.github.nichetoolkit.rest.configure.RestAesProperties
+     */
     private static synchronized String encrypts(String source, RestAesProperties properties) {
         return encrypts(source, properties.getSecretKey(), properties.getSecretIv());
     }
 
+    /**
+     * <code>encrypts</code>
+     * <p>The encrypts method.</p>
+     * @param source {@link java.lang.String} <p>The source parameter is <code>String</code> type.</p>
+     * @return {@link java.lang.String} <p>The encrypts return object is <code>String</code> type.</p>
+     * @see java.lang.String
+     */
     public static synchronized String encrypts(String source) {
         return encrypts(source, INSTANCE.aesProperties.getSecretKey(), INSTANCE.aesProperties.getSecretIv());
     }
 
+    /**
+     * <code>encrypts</code>
+     * <p>The encrypts method.</p>
+     * @param source   {@link java.lang.String} <p>The source parameter is <code>String</code> type.</p>
+     * @param secretIv {@link java.lang.String} <p>The secret iv parameter is <code>String</code> type.</p>
+     * @return {@link java.lang.String} <p>The encrypts return object is <code>String</code> type.</p>
+     * @see java.lang.String
+     */
     public static synchronized String encrypts(String source, String secretIv) {
         return encrypts(source, INSTANCE.aesProperties.getSecretKey(), secretIv);
     }
 
+    /**
+     * <code>encrypts</code>
+     * <p>The encrypts method.</p>
+     * @param source    {@link java.lang.String} <p>The source parameter is <code>String</code> type.</p>
+     * @param secretKey {@link java.lang.String} <p>The secret key parameter is <code>String</code> type.</p>
+     * @param secretIv  {@link java.lang.String} <p>The secret iv parameter is <code>String</code> type.</p>
+     * @return {@link java.lang.String} <p>The encrypts return object is <code>String</code> type.</p>
+     * @see java.lang.String
+     */
     public static synchronized String encrypts(String source, String secretKey, String secretIv) {
         try {
             SecretKeySpec keySpec = new SecretKeySpec(secretKey.getBytes(), ALGORITHM);
@@ -78,18 +168,51 @@ public class AesWorker {
         return null;
     }
 
+    /**
+     * <code>decrypts</code>
+     * <p>The decrypts method.</p>
+     * @param target     {@link java.lang.String} <p>The target parameter is <code>String</code> type.</p>
+     * @param properties {@link io.github.nichetoolkit.rest.configure.RestAesProperties} <p>The properties parameter is <code>RestAesProperties</code> type.</p>
+     * @return {@link java.lang.String} <p>The decrypts return object is <code>String</code> type.</p>
+     * @see java.lang.String
+     * @see io.github.nichetoolkit.rest.configure.RestAesProperties
+     */
     private static synchronized String decrypts(String target, RestAesProperties properties) {
         return decrypts(target, properties.getSecretKey(), properties.getSecretIv());
     }
 
+    /**
+     * <code>decrypts</code>
+     * <p>The decrypts method.</p>
+     * @param target {@link java.lang.String} <p>The target parameter is <code>String</code> type.</p>
+     * @return {@link java.lang.String} <p>The decrypts return object is <code>String</code> type.</p>
+     * @see java.lang.String
+     */
     public static synchronized String decrypts(String target) {
         return decrypts(target, INSTANCE.aesProperties.getSecretKey(), INSTANCE.aesProperties.getSecretIv());
     }
 
+    /**
+     * <code>decrypts</code>
+     * <p>The decrypts method.</p>
+     * @param target   {@link java.lang.String} <p>The target parameter is <code>String</code> type.</p>
+     * @param secretIv {@link java.lang.String} <p>The secret iv parameter is <code>String</code> type.</p>
+     * @return {@link java.lang.String} <p>The decrypts return object is <code>String</code> type.</p>
+     * @see java.lang.String
+     */
     public static synchronized String decrypts(String target, String secretIv) {
         return decrypts(target, INSTANCE.aesProperties.getSecretKey(), secretIv);
     }
 
+    /**
+     * <code>decrypts</code>
+     * <p>The decrypts method.</p>
+     * @param target    {@link java.lang.String} <p>The target parameter is <code>String</code> type.</p>
+     * @param secretKey {@link java.lang.String} <p>The secret key parameter is <code>String</code> type.</p>
+     * @param secretIv  {@link java.lang.String} <p>The secret iv parameter is <code>String</code> type.</p>
+     * @return {@link java.lang.String} <p>The decrypts return object is <code>String</code> type.</p>
+     * @see java.lang.String
+     */
     public static synchronized String decrypts(String target, String secretKey, String secretIv) {
         try {
             SecretKeySpec keySpec = new SecretKeySpec(secretKey.getBytes(), ALGORITHM);
